aa5214166eca93bb54d8609e2df9dd13eeb30e93
Val <foraacs@meta.ua>
2022-06-23 18:32:14 +0300

    ffix

--- src/components/subscription-form.vue
+++ src/components/subscription-form.vue
@@ -56,7 +56,7 @@
 
                 const response = await fetch('https://bulkhulk.vercel.app/send_mail/', {
                     method: 'POST',
-                    body: JSON.stringify(form),
+                    body: new FormData(form),
                     headers: {
                         "Content-Type": "application/json; charset=UTF-8",
                     },
